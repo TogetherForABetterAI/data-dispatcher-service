02af13cd0084f03026a11c098ca60c7fea13a7ca
tinchomorilla <mmorilla@fi.uba.ar>
2025-07-02 23:59:03 -0300

    refactor gRPC client connection initialization

--- src/grpc/client.go
+++ src/grpc/client.go
@@ -1,27 +1,27 @@
 package grpc
 
 import (
-    "context"
-    "google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"context"
+
 	pb "github.com/mlops-eval/data-dispatcher-service/src/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-
 type Client struct {
-    conn   *grpc.ClientConn
-    client pb.DatasetServiceClient
+	conn   *grpc.ClientConn
+	client pb.DatasetServiceClient
 }
 
 func NewClient(address string) (*Client, error) {
-    conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        return nil, err
-    }
-    client := pb.NewDatasetServiceClient(conn)
-    return &Client{conn: conn, client: client}, nil
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	client := pb.NewDatasetServiceClient(conn)
+	return &Client{conn: conn, client: client}, nil
 }
 
 func (c *Client) GetBatch(ctx context.Context, req *pb.GetBatchRequest) (*pb.DataBatch, error) {
-    return c.client.GetBatch(ctx, req)
+	return c.client.GetBatch(ctx, req)
 }
