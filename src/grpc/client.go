c48e9e8fcf4dbcfb0c6262ef36bc687a76c352d6
tinchomorilla <mmorilla@fi.uba.ar>
2025-10-10 17:11:51 -0300

    update import path

--- src/grpc/client.go
+++ src/grpc/client.go
@@ -3,7 +3,7 @@ package grpc
 import (
 	"context"
 	"fmt"
-	datasetpb "github.com/mlops-eval/data-dispatcher-service/src/pb/dataset-service"
+	datasetpb "github.com/mlops-eval/data-dispatcher-service/src/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
